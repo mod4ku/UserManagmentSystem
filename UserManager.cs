b5e077ff07fe6c27ad62e6af3a99b801ed5c6d21
ISPO0122001 Абдишев Эмиль Амирович <ISPO0122001@sibupk.su>
2025-04-24 19:50:14 +0700

    Добавлен базовый класс UserManager

---
+++ UserManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManagmentSystem
+{
+    public class UserManager
+    {
+        private List _users = new List();
+
+        public void AddUser(string username)
+        {
+            _users.Add(username);
+        }
+    }
+}
