2c992a83ded92c1fd489ba9486b7b365b57ed64b
ispo0122001 <ispo0122001@CL326-05>
2025-04-24 19:47:41 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserManagmentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
